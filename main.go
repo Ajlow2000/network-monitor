3b2025a21ba70986321d5fe6bd1cc244028c16fc
Ajlow2000 <ajlow2000@gmail.com>
2022-11-17 12:14:14 -0600

    Email notifications in place

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/exec"
+	"strings"
 	"text/template"
 	"time"
 
@@ -35,9 +38,19 @@ const wanTarget = "8.8.8.8"
 const INTERVAL = 5
 
 type Event struct {
+	Loc   string
 	Desc  string
 	Start time.Time
 	End   time.Time
+}
+
+func pingCmd(target string) bool {
+	out, _ := exec.Command("ping", "8.8.8.8", "-c 1").Output()
+	if strings.Contains(string(out), "64 bytes from "+wanTarget) {
+		return true
+	} else {
+		return false
+	}
 }
 
 func ping(target string) bool {
@@ -64,21 +77,37 @@ func ping(target string) bool {
 	return result
 }
 
-func notify(e Event) {
+func emailNotify(e Event) {
+	type prettyEvent struct {
+		Loc      string
+		Desc     string
+		Start    string
+		End      string
+		Duration string
+	}
+
+	pe := prettyEvent{
+		e.Loc,
+		e.Desc,
+		e.Start.Format(time.RFC1123),
+		e.End.Format(time.RFC1123),
+		fmt.Sprintf("%f", e.End.Sub(e.Start).Minutes()),
+	}
+
 	var body bytes.Buffer
 	t, err := template.ParseFiles("email.html")
 	if err != nil {
 		Error_Level.Printf("Error opening email.html template")
 		return
 	}
-	t.Execute(&body, e)
+	t.Execute(&body, pe)
 
 	msg := gomail.NewMessage()
 	msg.SetHeader("From", "ajlow2000.api@gmail.com")
-	msg.SetHeader("To", "junkmail00310@gmail.com")
-	msg.SetHeader("Subject", e.Desc)
+	msg.SetHeader("To", "ajlow2000.api@gmail.com")
+	msg.SetHeader("Subject", e.Desc+" at "+getSSID())
 	msg.SetBody("text/html", body.String())
-	// msg.Attach("/home/User/cat.jpg")
+	msg.Attach("network-monitor.log")
 
 	n := gomail.NewDialer("smtp.gmail.com", 587, "ajlow2000.api@gmail.com", "urtpnabjocusjdwe")
 
@@ -86,38 +115,69 @@ func notify(e Event) {
 	if err := n.DialAndSend(msg); err != nil {
 		Error_Level.Printf(err.Error())
 	}
+	Event_Level.Printf("Email Sent")
+}
+
+func getSSID() string {
+	cmd := exec.Command("nmcli", "connection", "show", "--active")
+
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	scan := bufio.NewScanner(out)
+	for scan.Scan() {
+		line := scan.Text()
+		if !strings.Contains(line, "wifi") {
+			continue
+		}
+
+		parts := strings.SplitN(line, " ", 2)
+		return parts[0]
+	}
+
+	if err := scan.Err(); err != nil {
+		Error_Level.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		Error_Level.Fatal(err)
+	}
+	return ""
 }
 
 func main() {
-	notify(Event{"TEST NOTIF", time.Now(), time.Time{}})
-	// var downtime_start time.Time
-	// var downtime_end time.Time
+	var downtime_start time.Time
+	var downtime_end time.Time
 
-	// log.Printf("Beginning monitor")
-	// for {
-	// 	result := ping(wanTarget)
-	// 	if !result {
-	// 		Info_Level.Printf("%v Unreachable", wanTarget)
-	// 		if downtime_start.IsZero() {
-	// 			downtime_start = time.Now()
-	// 			e := Event{"Outage Detected", downtime_start, downtime_end}
-	// 			Event_Level.Printf(e.Desc)
-	// 			notify(e)
-	// 		}
-	// 	} else {
-	// 		Info_Level.Printf("%v Received", wanTarget)
-	// 		if !downtime_start.IsZero() && downtime_end.IsZero() {
-	// 			downtime_end = time.Now()
-	// 			duration := downtime_end.Sub(downtime_start)
+	log.Printf("Beginning monitor")
+	for {
+		result := pingCmd(wanTarget)
+		if !result {
+			Info_Level.Printf("%v Unreachable", wanTarget)
+			if downtime_start.IsZero() {
+				downtime_start = time.Now()
+				Event_Level.Printf("Outage Detected")
+			}
+		} else {
+			Info_Level.Printf("%v Received", wanTarget)
+			if !downtime_start.IsZero() && downtime_end.IsZero() {
+				downtime_end = time.Now()
+				duration := downtime_end.Sub(downtime_start)
 
-	// 			e := Event{"Outage Resolved", downtime_start, downtime_end}
-	// 			Event_Level.Printf(e.Desc+" - Duration (seconds): %v", duration.Seconds())
-	// 			notify(e)
+				e := Event{getSSID(), "Outage Resolved", downtime_start, downtime_end}
+				Event_Level.Printf(e.Desc+" - Duration (seconds): %v", duration.Seconds())
+				emailNotify(e)
 
-	// 			downtime_start = time.Time{}
-	// 			downtime_end = time.Time{}
-	// 		}
-	// 	}
-	// 	time.Sleep(time.Second * INTERVAL)
-	// }
+				downtime_start = time.Time{}
+				downtime_end = time.Time{}
+			}
+		}
+		time.Sleep(time.Second * INTERVAL)
+	}
 }
