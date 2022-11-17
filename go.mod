7dd22580f4f30f8bb38d82e63409f3c5fe22623e
Alec Lowry <39601074+Ajlow2000@users.noreply.github.com>
2022-11-17 13:26:10 -0600

    Event Notification by Email (#3)
    * Basic structure for email notif added
    
    * Basic email functionality added
    
    * Email notifications in place
    
    * Removed public password and rewrote with env var
    
    * Added full env var support
    
    Co-authored-by: Ajlow2000 <ajlow2000@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.18
 
 require (
 	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e // indirect
+	github.com/yelinaung/wifi-name v0.0.0-20181205043121-60d8acb81b8f // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/exec"
+	"strings"
+	"text/template"
 	"time"
 
 	"github.com/tatsushid/go-fastping"
+	"gopkg.in/gomail.v2"
+	// requires iwgetid util
 )
 
 var (
 	Info_Level  *log.Logger
 	Event_Level *log.Logger
+	Error_Level *log.Logger
 )
 
 func init() {
@@ -23,15 +31,26 @@ func init() {
 
 	Info_Level = log.New(file, "INFO : ", log.Ldate|log.Ltime)
 	Event_Level = log.New(file, "EVENT: ", log.Ldate|log.Ltime)
+	Error_Level = log.New(file, "ERROR: ", log.Ldate|log.Ltime)
 }
 
 const wanTarget = "8.8.8.8"
 const INTERVAL = 5
 
 type Event struct {
+	Loc   string
 	Desc  string
 	Start time.Time
 	End   time.Time
+}
+
+func pingCmd(target string) bool {
+	out, _ := exec.Command("ping", "8.8.8.8", "-c 1").Output()
+	if strings.Contains(string(out), "64 bytes from "+wanTarget) {
+		return true
+	} else {
+		return false
+	}
 }
 
 func ping(target string) bool {
@@ -58,13 +77,87 @@ func ping(target string) bool {
 	return result
 }
 
+func emailNotify(e Event) {
+	type prettyEvent struct {
+		Loc      string
+		Desc     string
+		Start    string
+		End      string
+		Duration string
+	}
+
+	pe := prettyEvent{
+		e.Loc,
+		e.Desc,
+		e.Start.Format(time.RFC1123),
+		e.End.Format(time.RFC1123),
+		fmt.Sprintf("%f", e.End.Sub(e.Start).Minutes()),
+	}
+
+	var body bytes.Buffer
+	t, err := template.ParseFiles("email.html")
+	if err != nil {
+		Error_Level.Printf("Error opening email.html template")
+		return
+	}
+	t.Execute(&body, pe)
+
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", os.Getenv("NM_API_EMAIL"))
+	msg.SetHeader("To", os.Getenv("NM_RECIPIENT_EMAIL"))
+	msg.SetHeader("Subject", e.Desc+" at "+getSSID())
+	msg.SetBody("text/html", body.String())
+	msg.Attach("network-monitor.log")
+
+	n := gomail.NewDialer("smtp.gmail.com", 587, os.Getenv("NM_API_EMAIL"), os.Getenv("NM_API_EMAIL_PASSWORD"))
+
+	// Send the email
+	if err := n.DialAndSend(msg); err != nil {
+		Error_Level.Printf(err.Error())
+	}
+	log.Println("Email Sent to " + os.Getenv("NM_RECIPIENT_EMAIL"))
+}
+
+func getSSID() string {
+	cmd := exec.Command("nmcli", "connection", "show", "--active")
+
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	scan := bufio.NewScanner(out)
+	for scan.Scan() {
+		line := scan.Text()
+		if !strings.Contains(line, "wifi") {
+			continue
+		}
+
+		parts := strings.SplitN(line, " ", 2)
+		return parts[0]
+	}
+
+	if err := scan.Err(); err != nil {
+		Error_Level.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		Error_Level.Fatal(err)
+	}
+	return ""
+}
+
 func main() {
 	var downtime_start time.Time
 	var downtime_end time.Time
 
-	log.Printf("Beginning monitor")
+	log.Printf("Beginning network-monitor")
 	for {
-		result := ping(wanTarget)
+		result := pingCmd(wanTarget)
 		if !result {
 			Info_Level.Printf("%v Unreachable", wanTarget)
 			if downtime_start.IsZero() {
@@ -77,7 +170,9 @@ func main() {
 				downtime_end = time.Now()
 				duration := downtime_end.Sub(downtime_start)
 
-				Event_Level.Printf("Outage Resolved - Duration (seconds): %v", duration.Seconds())
+				e := Event{getSSID(), "Outage Resolved", downtime_start, downtime_end}
+				Event_Level.Printf(e.Desc+" - Duration (seconds): %v", duration.Seconds())
+				emailNotify(e)
 
 				downtime_start = time.Time{}
 				downtime_end = time.Time{}
